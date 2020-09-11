2c9b97422c4d93d3093304bcb487e75d40d1ce1b
Greg Frediani <fredianig11@gmail.com>
2020-09-11 12:40:22 +0100

    basic auth & local envs

---
+++ src/store/actions/auth.js
@@ -0,0 +1,47 @@
+import * as actionTypes from './actionTypes';
+import axios from 'axios';
+
+export const authStart = () => {
+  return {
+    type: actionTypes.AUTH_START
+  };
+};
+
+export const authSuccess = authData => {
+  return {
+    type: actionTypes.AUTH_SUCCESSS,
+    authData
+  };
+};
+
+export const authFail = error => {
+  return {
+    type: actionTypes.AUTH_FAIL,
+    error
+  };
+};
+
+export const auth = (email, password) => {
+  return dispatch => {
+    dispatch(authStart());
+    const authData = {
+      email,
+      password,
+      returnSecureToken: true
+    };
+    axios
+      .post(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process
+          .env.REACT_APP_FIREBASE_API_KEY}`,
+        authData
+      )
+      .then(res => {
+        console.log(res);
+        dispatch(authSuccess(res.data));
+      })
+      .catch(err => {
+        console.log(err);
+        dispatch(authFail(err));
+      });
+  };
+};
